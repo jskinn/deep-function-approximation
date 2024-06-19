69fad4d1ad683f956a277af1a478bbe222282901
John Skinner <jskinn7@gmail.com>
2024-06-19 16:17:11 +1000

    Log the model summary on fit start.

--- deep_function_approximation/training_modules/vector_function_training_module.py
+++ deep_function_approximation/training_modules/vector_function_training_module.py
@@ -1,6 +1,8 @@
 from typing import Literal
+from pathlib import Path
 import torch
 import torch.nn as nn
+from torchinfo import summary
 from lightning import LightningModule
 from lightning.pytorch.loggers import MLFlowLogger
 import matplotlib.pyplot as plt
@@ -22,7 +24,19 @@ class VectorFunctionTrainingModule(LightningModule):
 
     @property
     def mlflow_logger(self) -> MLFlowLogger | None:
-        return next((logger for logger in self.loggers if isinstance(logger, MLFlowLogger)), None)
+        return next(
+            (logger for logger in self.loggers if isinstance(logger, MLFlowLogger)),
+            None,
+        )
+
+    def on_fit_start(self) -> None:
+        logger = self.mlflow_logger
+        if logger is not None:
+            summary_path = Path("model_summary.txt")
+            with summary_path.open("w") as fp:
+                fp.write(str(summary(self.network)))
+            logger.experiment.log_artifact(logger.run_id, summary_path)
+            summary_path.unlink()
 
     def training_step(self, batch: FunctionBatch, batch_idx):
         return self._step(batch, "Training")
@@ -40,10 +54,7 @@ class VectorFunctionTrainingModule(LightningModule):
         self._log_plots()
 
     def _step(
-        self,
-        batch: FunctionBatch,
-        step_name: str = "Training",
-        visualise: bool = False
+        self, batch: FunctionBatch, step_name: str = "Training", visualise: bool = False
     ):
         x = batch.x.reshape(-1, batch.x.size(-1))
         y = batch.y.reshape(-1, batch.y.size(-1))
@@ -61,15 +72,9 @@ class VectorFunctionTrainingModule(LightningModule):
         mlflow_logger = self.mlflow_logger
         if mlflow_logger is None:
             return
-        x = torch.cat([
-            batch_data[0].cpu() for batch_data in self._vis_data
-        ], dim=0)
-        y = torch.cat([
-            batch_data[1].cpu() for batch_data in self._vis_data
-        ], dim=0)
-        preds = torch.cat([
-            batch_data[2].cpu() for batch_data in self._vis_data
-        ], dim=0)
+        x = torch.cat([batch_data[0].cpu() for batch_data in self._vis_data], dim=0)
+        y = torch.cat([batch_data[1].cpu() for batch_data in self._vis_data], dim=0)
+        preds = torch.cat([batch_data[2].cpu() for batch_data in self._vis_data], dim=0)
         self._vis_data = []
 
         num_inputs = x.size(-1)
@@ -78,10 +83,22 @@ class VectorFunctionTrainingModule(LightningModule):
         for x_idx in range(num_inputs):
             for y_idx in range(num_outputs):
                 y_range = min(3 * float(y[:, y_idx].std()), 10)
-                axes[x_idx][y_idx].plot(x[:, x_idx], y[:, y_idx], markersize=1, linestyle="none", marker='.')
-                axes[x_idx][y_idx].plot(x[:, x_idx], preds[:, y_idx], markersize=1, linestyle="none", marker='.')
+                axes[x_idx][y_idx].plot(
+                    x[:, x_idx], y[:, y_idx], markersize=1, linestyle="none", marker="."
+                )
+                axes[x_idx][y_idx].plot(
+                    x[:, x_idx],
+                    preds[:, y_idx],
+                    markersize=1,
+                    linestyle="none",
+                    marker=".",
+                )
                 axes[x_idx][y_idx].set_ylim(-1 * y_range, 1 * y_range)
-        mlflow_logger.experiment.log_figure(mlflow_logger.run_id, fig, f"true_vs_predicted_epoch{self.current_epoch}.png")
+        mlflow_logger.experiment.log_figure(
+            mlflow_logger.run_id,
+            fig,
+            f"true_vs_predicted_epoch{self.current_epoch}.png",
+        )
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.network.parameters(), lr=1e-4)
